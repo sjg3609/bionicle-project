3be12ec3a14641146428c46840622c9e9e6b3945
Sam Gossie <sjg3609@gmail.com>
2023-08-07 09:01:59 -0500

    Attempt to set up the LEGO API Key and server side routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const axios = require('axios');
 
 const app = express();
 
@@ -9,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const bionicleRouter = require('./routes/bionicle.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -21,8 +23,18 @@ app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/api/search/:id', (req, res) => {
+  axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.BIONICLE_API_KEY}&q=${req.params.id}&limit=25&offset=0&rating=g&lang=en`).then((response) => {
+    res.send(response.data);
+  }).catch((error) => {
+    console.log(error);
+    res.sendStatus(500);
+  })
+})
+
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/bionicle', bionicleRouter);
 
 // Serve static files
 app.use(express.static('build'));
