fbdfef113e0e5b83681d3906549821e68c863c7c
Sam Gossie <sjg3609@gmail.com>
2023-11-24 09:27:24 -0600

    Returning no 500 errors or 400 errors from API. But still returning parameter errors

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/api/bionicle/:id', (req, res) => {
-  axios.get(`/api/v3.asmx/getSets?apiKey=${process.env.BIONICLE_API_KEY}&userHash=${process.env.USER_HASH}&params=${req.params.id}`).then((response) => {
+  axios.get(`https://brickset.com/api/v3.asmx/getSets?apiKey=${process.env.BIONICLE_API_KEY}&userHash=${process.env.USER_HASH}&params=${req.params.id}`).then((response) => {
     console.log("This should be my data", response.data);
     res.send(response.data);
   }).catch((error) => {
